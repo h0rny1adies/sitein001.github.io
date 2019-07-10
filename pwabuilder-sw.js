6eb7e33d7dec799eb7e7f2ad5fc0967308268e18
sitein001 <52627412+sitein001@users.noreply.github.com>
2019-07-10 23:12:03 +0300

    1

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,5 @@
 self.addEventListener('install', function(event) {
-  var indexPage = new Request('index.html');
+  var indexPage = new Request('pwa-main.html');
   event.waitUntil(
     fetch(indexPage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
